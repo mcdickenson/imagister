262520219fa8ca22f728885912cb21813fd11f38
Matt Dickenson <mcdickenson@gmail.com>
2013-07-24 19:57:07 -0400

    started sideways-histogram.js for timeline

---
+++ js/sideways-histogram.js
@@ -0,0 +1,39 @@
+var counts = [30, 20, 10];
+
+var toLeftX = function(d){
+	return 100-d;
+}
+
+var toRightX = function(d){
+	return 100;
+}
+
+var leftOrRight = function(d){
+	if(d===30){ xPlacement = toLeftX(d); 
+	} else if(d===20){ xPlacement = toRightX(d); 
+	} else if(d===10){ xPlacement = toLeftX(d); }
+	return xPlacement;
+}
+
+var svgContainer = d3.select("body").append("svg")
+	.attr("width", 200)
+	.attr("height", 200);
+
+var line = svgContainer.append("line")
+	.attr("x1", 100)
+	.attr("y1", 10)
+	.attr("x2", 100)
+	.attr("y2", 70)
+	.attr("stroke-with", 2)
+	.attr("stroke", "black");
+
+var rects = svgContainer.selectAll("rect")
+	.data(counts)
+	.enter()
+	.append("rect");
+
+var rectAttributes = rects
+	.attr("x", leftOrRight )
+	.attr("y", function(d) { return d*2; } )
+	.attr("width", function(d) { return d; })
+	.attr("height", 10);
