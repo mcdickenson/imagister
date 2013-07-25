2873c160839047ee5c7a29429f26a33d498dfb72
Matt Dickenson <mcdickenson@gmail.com>
2013-07-24 22:24:39 -0400

    timeline now represents counts horizontally

--- js/timeline.js
+++ js/timeline.js
@@ -115,12 +115,37 @@ var daySeq = getDates(minDate, maxDate);
 var dayCounts = [];
 
 for(var i=0; i<daySeq.length; i++){
-	dayCounts[daySeq[i]] = 0;
+	dayCounts[i] = { "date": daySeq[i],
+									 "index": i,
+									 "count": 0 };
 }
 
 for(var i=0; i<records.length; i++){
-	var ix = (new Date(records[i].Date)).toDateString();
-	dayCounts[ix] += 1;
+	var ix = daySeq.indexOf((new Date(records[i].Date)).toDateString());
+	dayCounts[ix].count += 1;
 }
-console.log(dayCounts);
+// console.log(dayCounts);
+
+var svgContainer = d3.select("body").append("svg")
+	.attr("width", 200)
+	.attr("height", 200);
+
+var line = svgContainer.append("line")
+	.attr("x1", 100)
+	.attr("y1", 0)
+	.attr("x2", 100)
+	.attr("y2", 200)
+	.attr("stroke-with", 2)
+	.attr("stroke", "black");
+
+var rects = svgContainer.selectAll("rect")
+	.data(dayCounts)
+	.enter()
+	.append("rect");
+
+var rectAttributes = rects
+	.attr("x", 100)
+	.attr("y", function(d) { return d.index*10; } )
+	.attr("width", function(d) { return d.count*10; })
+	.attr("height", 10);
 
