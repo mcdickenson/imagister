159e465fa168529664a3a4d9acd748278debfacb
Matt Dickenson <mcdickenson@gmail.com>
2013-07-29 18:25:11 -0400

    add example with custom torque settings

--- js/mdcarto.js
+++ js/mdcarto.js
@@ -1,12 +1,14 @@
 var gui;
 function initialize(){
-	var user_name = "mcdickenson";
-	var table_name = "gdelt_icews_20130725";
+	// var user_name = "mcdickenson";
+	// var table_name = "gdelt_icews_20130725";
+	// var column_name = "date";
+	var user_name = "viz2";
+	var table_name = "ow";
 	var column_name = "date";
 
 	// google map
 	// centered on cairo
-	// todo: try out different base maps (dark)
 	var map = new google.maps.Map(document.getElementById('map_canvas'), {
 		center:new google.maps.LatLng(30.05, 31.226),
 		zoom:5,
@@ -47,6 +49,27 @@ function initialize(){
 
 	map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
 	map.setOptions({styles:map_style.google_maps_customization_style});
-	
+
+	var TorqueOptions = {
+		user: user_name,
+		table: table_name,
+		column: column_name,
+		cumulative: false,
+		resolution: 2,
+		fps: 12,
+		fitbounds: false,
+		clock: true,
+		blendmode: 'lighter',
+		trails: true,
+		point_type:'circle',
+		cellsize:1
+	}
+
+	var torque = null;
+	Torque(function(env){
+		Torque.app = new env.app.Instance();
+		torque = new Torque.app.addLayer(map, TorqueOptions);
+		Torque.env = env;
+	});
 
 }
