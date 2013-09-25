dad22b200bf49185e85e370e6df47a071b78ea46
Matt Dickenson <mcdickenson@gmail.com>
2013-09-25 13:03:24 -0400

    commit in order to push then pull

--- rcode/icews-syria.R
+++ rcode/icews-syria.R
@@ -1,14 +1,22 @@
+setwd("~/github/imagister/rcode")
 source("icews-functions.R")
 
-icewsSYR <- read.csv("C:/users/ben/dropbox/gdelt-egypt-turkey/EgyptSyriaTurkey-2010forward/Syria.csv",stringsAsFactors=F)
+# icewsSYR <- read.csv("C:/users/ben/dropbox/gdelt-egypt-turkey/EgyptSyriaTurkey-2010forward/Syria.csv",stringsAsFactors=F)
+setwd('~/dropbox/gdelt-egypt-turkey/EgyptSyriaTurkey-2010forward')
+icewsSYR <- read.csv("Syria.csv",stringsAsFactors=F)
+dim(icewsSYR)
 
 icewsSYR <- grepActor(c("Government","Military","Police"),"govt",icewsSYR)
 icewsSYR <- grepActor(c("Protestor","Opposition","Dissident","Rebel"),"anti",icewsSYR)
 
 icewsSYR <- grepAction(c("military force","blockade","Occupy territory","small arms and light weapons","artillery and tanks","aerial weapons","ceasefire" , "unconventional violence", "riot"),"fight",icewsSYR)
 
-newdata <- subsetICEWS("govt","fight","anti",icewsSYR,"Syria","2012-06-01")
-newdata <- rbind(newdata,subsetICEWS("anti","fight","govt",icewsSYR,"Syria","2012-06-01"))
+newdata <- subsetICEWS("govt","fight","anti",icewsSYR,"Syria","2011-01-01")
+newdata <- rbind(newdata,subsetICEWS("anti","fight","govt",icewsSYR,"Syria","2011-01-01"))
+dim(newdata)
+head(newdata)
 
 #write.csv(newdata,"../data/icews_syria.csv",row.names=F)
-icewsJSON(newdata,"../data/icews_syria.json")
+# icewsJSON(newdata,"../data/icews_syria.json")
+setwd("~/github/imagister/rcode")
+write.csv(newdata,"../data/icews-syria-2011on.csv",row.names=F)
--- rcode/icews-turkey.R
+++ rcode/icews-turkey.R
@@ -1,6 +1,10 @@
+setwd("~/github/imagister/rcode")
 source("icews-functions.R")
 
-icewsTUR <- read.csv("C:/users/ben/dropbox/gdelt-egypt-turkey/EgyptSyriaTurkey-2010forward/Turkey.csv",stringsAsFactors=F)
+# icewsTUR <- read.csv("C:/users/ben/dropbox/gdelt-egypt-turkey/EgyptSyriaTurkey-2010forward/Turkey.csv",stringsAsFactors=F)
+setwd('~/dropbox/gdelt-egypt-turkey/EgyptSyriaTurkey-2010forward')
+icewsTUR <- read.csv("Turkey.csv",stringsAsFactors=F)
+dim(icewsTUR)
 
 icewsTUR <- grepActor(c("Government","Military","Police"),"govt",icewsTUR)
 icewsTUR <- grepActor(c("Protestor","Opposition","Dissident","Rebel"),"anti",icewsTUR)
@@ -8,8 +12,11 @@ icewsTUR <- grepActor(c("Protestor","Opposition","Dissident","Rebel"),"anti",ice
 icewsTUR <- grepAction(c("riot","rally","Protest"),"protest",icewsTUR)
 icewsTUR <- grepAction(c("repression","military force","small arms"),"repress",icewsTUR)
 
-newdata <- subsetICEWS("govt","repress","anti",icewsTUR,"Turkey","2012-06-01")
-newdata <- rbind(newdata,subsetICEWS("anti","protest","govt",icewsTUR,"Turkey","2012-06-01"))
+newdata <- subsetICEWS("govt","repress","anti",icewsTUR,"Turkey","2011-01-01")
+newdata <- rbind(newdata,subsetICEWS("anti","protest","govt",icewsTUR,"Turkey","2011-01-01"))
 
 #write.csv(newdata,"../data/icews_turkey.csv",row.names=F)
-icewsJSON(newdata,"../data/icews_turkey.json")
+# icewsJSON(newdata,"../data/icews_turkey.json")
+
+setwd("~/github/imagister/rcode")
+write.csv(newdata,"../data/icews-turkey-2011on.csv",row.names=F)
